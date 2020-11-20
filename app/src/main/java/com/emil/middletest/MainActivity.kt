74c26ff54345a818dab695b0b8d4dd6ff7d89034
Scute0001 <s12t8811@gmail.com>
2020-11-20 10:15:13 +0800

    add navigation resource to homefragment

--- app/src/main/java/com/emil/middletest/MainActivity.kt
+++ app/src/main/java/com/emil/middletest/MainActivity.kt
@@ -2,11 +2,17 @@ package com.emil.middletest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.emil.middletest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+
     }
 
 
---
+++ app/src/main/java/com/emil/middletest/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.emil.middletest.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
